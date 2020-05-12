3404255a792021bbd9d034b3fa04062fd2da55bf
KarlaGT <kaarlytorrez@hotmail.com>
2020-05-11 21:33:26 -0500

    Yas

---
+++ CODEPEN/Registrarse.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+document.getElementsByTagName("body")[0].style.background = "#2D2D30";
+document.getElementsByTagName("body")[0].style.textAlign = "center"
+document.getElementsByTagName("body")[0].style.color = "white"
+document.getElementsByTagName("body")[0].style.color = "system-ui"
+document.getElementsByTagName("body")[0].style.margin = "50px"
+
+function capturar()
+{
+    var porElementos = document.forms["form0"].elements[0].value
+}
